50467cc72069940ca685d9f12c068c1d687bd8f9
Dominik Stadler <dominik.stadler@gmx.at>
2022-02-18 12:03:22 +0100

    Make it a bit harder to detect the condition by using three separate numbers

--- src/main/java/org/dstadler/fuzz/Fuzz.java
+++ src/main/java/org/dstadler/fuzz/Fuzz.java
@@ -12,11 +12,12 @@ public class Fuzz {
 	public static void fuzzerTestOneInput(FuzzedDataProvider data) {
 		int i = data.consumeInt(0, 10000000);
 		int j = data.consumeInt(0, 10000000);
+		int k = data.consumeInt(0, 10000000);
 
 		// Jazzer will quickly find that this special number
 		// is necessary to enter this branch and so fuzzing
 		// this method quickly throws the exception
-		if (i == 9999999 && j == 9999998) {
+		if (i == 9999999 && j == 9999998 && k == 9999998) {
 			throw new IllegalStateException("Expected exception if Fuzzing manages to 'detect' the values that enters"
 					+ " the exception-throwing branch.");
 		}
