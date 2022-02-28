b41c63a8597af646dbe8937dc8bb72c614cd1e0c
Dominik Stadler <dominik.stadler@gmx.at>
2022-02-28 17:21:12 +0100

    Add two Fuzz targets to show usage with byte-array and with data-provider

--- src/main/java/org/dstadler/fuzz/Fuzz.java
+++ src/main/java/org/dstadler/fuzz/Fuzz.java
@@ -1,6 +1,6 @@
 package org.dstadler.fuzz;
 
-import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import java.nio.charset.StandardCharsets;
 
 /**
  * This class provides a simple target for fuzzing which
@@ -9,15 +9,12 @@ import com.code_intelligence.jazzer.api.FuzzedDataProvider;
  * to visit branches
  */
 public class Fuzz {
-	public static void fuzzerTestOneInput(FuzzedDataProvider data) {
-		int i = data.consumeInt(0, 10000000);
-		int j = data.consumeInt(0, 10000000);
-		int k = data.consumeInt(0, 10000000);
+	public static void fuzzerTestOneInput(byte[] input) {
+		String value = new String(input, StandardCharsets.UTF_8);
 
-		// Jazzer will quickly find that this special number
-		// is necessary to enter this branch and so fuzzing
-		// this method quickly throws the exception
-		if (i == 9999999 && j == 9999998 && k == 9999998) {
+		// Jazzer will quickly find that the string needs to match
+		// some values
+		if (value.contains("TEST") && value.contains("FOO") && value.contains("BAR") && value.contains("BAZ")) {
 			throw new IllegalStateException("Expected exception if Fuzzing manages to 'detect' the values that enters"
 					+ " the exception-throwing branch.");
 		}
---
+++ src/main/java/org/dstadler/fuzz/FuzzWithProvider.java
@@ -0,0 +1,25 @@
+package org.dstadler.fuzz;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+
+/**
+ * This class provides a simple target for fuzzing which
+ * shows that Jazzer does not use simple brute-force, but
+ * rather uses coverage data to find inputs that are needed
+ * to visit branches
+ */
+public class FuzzWithProvider {
+	public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+		int i = data.consumeInt(0, 10000000);
+		int j = data.consumeInt(0, 10000000);
+		int k = data.consumeInt(0, 10000000);
+
+		// Jazzer will quickly find that this special number
+		// is necessary to enter this branch and so fuzzing
+		// this method quickly throws the exception
+		if (i == 9999999 && j == 9999998 && k == 9999998) {
+			throw new IllegalStateException("Expected exception if Fuzzing manages to 'detect' the values that enters"
+					+ " the exception-throwing branch.");
+		}
+	}
+}
